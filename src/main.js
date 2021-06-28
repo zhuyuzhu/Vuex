7af4a90cc75e3d36173c35a04f51d340e2f72541
zhuyuzhu <struggle_zhu@163.com>
2021-06-28 19:42:26 +0800

    feat 模块化

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ let vm = new Vue({
     computed: {
         num() {
             console.log(1111)
-            return this.$store.state.count;
+            return this.$store.state.a.count;
         },
         num10 () {
             return this.$store.getters.count10; //Getter 会暴露为 store.getters 对象
@@ -29,9 +29,25 @@ let vm = new Vue({
         add() {
             //this.$store.commit('increment') //this.$store代表了vm对象上的属性
             //this.computedFunc('zyz')
-            this.$store.getters.todo;
-            this.num;
+            this.$store.commit('b/changeFirstName')
+            this.$store.dispatch('b/act')
+            console.log(this.$store.getters['b/fullName'])
+            
         }
     }
 })
 
+// 使用 Mock
+var data = Mock.mock('http://localhost:52330/getdata',{
+    // 属性 list 的值是一个数组，其中含有 1 到 10 个元素
+    'list|1-10': [{
+        // 属性 id 是一个自增数，起始值为 1，每次增 1
+        'id|+1': 1
+    }]
+})
+// 输出结果
+axios.get('http://localhost:52330/getdata').then(
+    (res) => {
+        console.log(res.data.list)
+    }
+)
--- src/router/router.js
+++ src/router/router.js
@@ -1,22 +1,64 @@
-const store = new Vuex.Store({
-    state: {
-      count: 0,
-    },
-    getters: {
-      count10: state => 10*(state.count),
-      todo (state, getters) {
-        console.log(getters.count10)
-      }
-    },
-    mutations: {
-      increment(state) {
-          state.count ++;
-      }
-    },
-    actions: {
-      increment(context) {
-        context.commit('increment')
-      }
+const moduleA = {
+  state: {
+    count: 0,
+  },
+  getters: {
+    count10: state => 10*(state.count),
+    todo (state, getters) {
+      console.log(getters.count10)
     }
+  },  
+  mutations: {
+    increment(state) {
+        state.count ++;
+    }
+  },
+  actions: {
+    increment(context) {
+      context.commit('increment')
+    }
+  }
+}
+
+const moduleB = {
+  namespaced: true,
+  state: () => ({ 
+    firstName: 'yuzhu',
+    lastName: 'zhu'
+   }),
+  getters: {
+    fullName: (state, getters, rootState, rootGetters) => {
+      console.log(rootState)
+      console.log(rootGetters.global10count)
+      return state.firstName + state.lastName;
+    }
+  },
+  mutations: { 
+    changeFirstName (state) {//局部state
+      state.firstName = 'daya'
+      console.log(state.firstName)
+    }
+   },
+  actions: { 
+    act ({ state, commit, rootState }) {//局部context多了一个属性：rootState，代表根节点状态
+      console.log(rootState)
+    }
+   }
+}
+
+const store = new Vuex.Store({
+  modules: {
+    a: moduleA,
+    b: moduleB
+  },
+  state: {
+    globalCount: 100
+  },
+  getters: {
+    global10count (state) {
+      return state.globalCount * 10;
+    }
+  }
 })
+
 window.store = store;
